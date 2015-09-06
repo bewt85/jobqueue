a91f287d354f72a20d026c36c8fc91846cea4d3e
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-09-06 19:57:31 +0100

    Add leases

--- argqueue/queue.py
+++ argqueue/queue.py
@@ -12,6 +12,35 @@ class Queue(object):
                   "Arguments(Id INTEGER PRIMARY KEY AUTOINCREMENT, "
                             "Args TEXT, Status TEXT, "
                             "Created INTEGER)")
+      cur.execute("CREATE TABLE IF NOT EXISTS "
+                  "Leases(Id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                         "Created INTEGER NOT NULL, "
+                         "Expires INTEGER NOT NULL, "
+                         "Status TEXT DEFAULT 'LET', "
+                         "ArgumentsId INTEGER NOT NULL, "
+                         "FOREIGN KEY(ArgumentsId) REFERENCES Arguments(Id))")
+
+  def _create_lease(self, arg_id, timeout):
+    with self.con:
+      cur = self.con.cursor()
+      cur.execute("INSERT INTO Leases (Created, Expires, ArgumentsId) "
+                  "VALUES (strftime('%s', 'now'), "
+                          "strftime('%s', 'now') + ?, "
+                          "?)", (timeout, arg_id))
+
+  def lease(self):
+    arg_id, args = self._pop_with_id(status='LEASED')
+    self._create_lease(arg_id, 3600)
+    return arg_id
+
+  def get(self, arg_id):
+    with self.con:
+      cur = self.con.cursor()
+      cur.execute("SELECT (Args) "
+                  "FROM Arguments "
+                  "WHERE Id=?", (arg_id,))
+      (args,) = cur.fetchone()
+      return args
 
   def put(self, arguments):
     with self.con:
@@ -19,7 +48,7 @@ class Queue(object):
       cur.execute("INSERT INTO Arguments (Args, Status, Created) "
                   "VALUES (?,'PENDING', strftime('%s', 'now'))", (arguments,))
 
-  def pop(self):
+  def _pop_with_id(self, status='UNKNOWN'):
     with self.con:
       cur = self.con.cursor()
       cur.execute("BEGIN EXCLUSIVE")
@@ -29,5 +58,9 @@ class Queue(object):
         row_id, args = cur.fetchone()
       except TypeError:
         raise Exception("No more arguments to pop")
-      cur.execute("UPDATE Arguments SET Status='UNKNOWN' WHERE Id=?", (row_id,))
-      return args
+      cur.execute("UPDATE Arguments SET Status=? WHERE Id=?", (status, row_id))
+      return row_id, args
+
+  def pop(self, status='UNKNOWN'):
+    row_id, args = self._pop_with_id(status=status)
+    return args
